4103e7aa5fca4b80dcfb2341730d84d6a9945487
olliee2 <218727432+olliee2@users.noreply.github.com>
2025-07-03 21:28:09 +0100

    Refactor Renderer and index files to include additional button elements and enhance battle log functionality

--- dist/Renderer.js
+++ dist/Renderer.js
@@ -1,5 +1,5 @@
 export default class Renderer {
-    constructor(engine, playerName, playerHP, playerImage, opponentName, opponentHP, opponentImage) {
+    constructor(engine, playerName, playerHP, playerImage, opponentName, opponentHP, opponentImage, mainButtons, moveButton, switchButton, moveButtons, move1Button, move2Button, move3Button, move4Button, moveBackButton, switchButtons, switch1Button, switch2Button, switch3Button, switch4Button, switch5Button, switch6Button, switchBackButton, battleLog) {
         this.engine = engine;
         this.playerName = playerName;
         this.playerHP = playerHP;
@@ -7,9 +7,27 @@ export default class Renderer {
         this.opponentName = opponentName;
         this.opponentHP = opponentHP;
         this.opponentImage = opponentImage;
+        this.mainButtons = mainButtons;
+        this.moveButton = moveButton;
+        this.switchButton = switchButton;
+        this.moveButtons = moveButtons;
+        this.move1Button = move1Button;
+        this.move2Button = move2Button;
+        this.move3Button = move3Button;
+        this.move4Button = move4Button;
+        this.moveBackButton = moveBackButton;
+        this.switchButtons = switchButtons;
+        this.switch1Button = switch1Button;
+        this.switch2Button = switch2Button;
+        this.switch3Button = switch3Button;
+        this.switch4Button = switch4Button;
+        this.switch5Button = switch5Button;
+        this.switch6Button = switch6Button;
+        this.switchBackButton = switchBackButton;
+        this.battleLog = battleLog;
     }
     log() {
-        console.log(this.engine, this.playerName, this.playerHP, this.playerImage, this.opponentName, this.opponentHP, this.opponentImage);
+        console.log(this.engine, this.playerName, this.playerHP, this.playerImage, this.opponentName, this.opponentHP, this.opponentImage, this.mainButtons, this.moveButton, this.switchButton, this.moveButtons, this.move1Button, this.move2Button, this.move3Button, this.move4Button, this.moveBackButton, this.switchButtons, this.switch1Button, this.switch2Button, this.switch3Button, this.switch4Button, this.switch5Button, this.switch6Button, this.switchBackButton, this.battleLog);
     }
 }
 //# sourceMappingURL=Renderer.js.map
--- src/index.ts
+++ src/index.ts
@@ -14,6 +14,52 @@ const opponentHP = document.getElementById(
 const opponentImage = document.getElementById(
   'opponent-image',
 )! as HTMLImageElement;
+const mainButtons = document.getElementById('main-buttons')! as HTMLDivElement;
+const moveButton = document.getElementById('move-button')! as HTMLButtonElement;
+const switchButton = document.getElementById(
+  'switch-button',
+)! as HTMLButtonElement;
+const moveButtons = document.getElementById('move-buttons')! as HTMLDivElement;
+const move1Button = document.getElementById(
+  'move-1-button',
+)! as HTMLButtonElement;
+const move2Button = document.getElementById(
+  'move-2-button',
+)! as HTMLButtonElement;
+const move3Button = document.getElementById(
+  'move-3-button',
+)! as HTMLButtonElement;
+const move4Button = document.getElementById(
+  'move-4-button',
+)! as HTMLButtonElement;
+const moveBackButton = document.getElementById(
+  'move-back-button',
+)! as HTMLButtonElement;
+const switchButtons = document.getElementById(
+  'switch-buttons',
+)! as HTMLDivElement;
+const switch1Button = document.getElementById(
+  'switch-1-button',
+)! as HTMLButtonElement;
+const switch2Button = document.getElementById(
+  'switch-2-button',
+)! as HTMLButtonElement;
+const switch3Button = document.getElementById(
+  'switch-3-button',
+)! as HTMLButtonElement;
+const switch4Button = document.getElementById(
+  'switch-4-button',
+)! as HTMLButtonElement;
+const switch5Button = document.getElementById(
+  'switch-5-button',
+)! as HTMLButtonElement;
+const switch6Button = document.getElementById(
+  'switch-6-button',
+)! as HTMLButtonElement;
+const switchBackButton = document.getElementById(
+  'switch-back-button',
+)! as HTMLButtonElement;
+const battleLog = document.getElementById('battle-log')! as HTMLOListElement;
 
 const engine = new BattleEngine(pokemonData.slice(0, 6), pokemonData.slice(6));
 const renderer = new Renderer(
@@ -24,6 +70,24 @@ const renderer = new Renderer(
   opponentName,
   opponentHP,
   opponentImage,
+  mainButtons,
+  moveButton,
+  switchButton,
+  moveButtons,
+  move1Button,
+  move2Button,
+  move3Button,
+  move4Button,
+  moveBackButton,
+  switchButtons,
+  switch1Button,
+  switch2Button,
+  switch3Button,
+  switch4Button,
+  switch5Button,
+  switch6Button,
+  switchBackButton,
+  battleLog,
 );
 engine.log();
 renderer.log();
